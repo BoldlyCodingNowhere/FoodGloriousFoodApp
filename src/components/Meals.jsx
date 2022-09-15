e3a1e6b59fc180a22e213bdecb266a68ccec4cc1
Andrew <65864103+BoldlyCodingNowhere@users.noreply.github.com>
2022-09-15 06:11:37 +0000

    finish Meal , add Search, start Modal components

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,19 +1,30 @@
 import { useGlobalContext } from '../context'
-
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 
 const Meals = () => {
-  const { meals } = useGlobalContext();
+  const { loading, meals } = useGlobalContext();
+  if (loading) {
+    return <section className="section">
+      <h4>Loading...</h4>
+    </section>
+  }
+
+  if (meals.length < 1) {
+    return <section className="section">
+      <h4>No meals matched your search term. Please try again.</h4>
+    </section>  
+  }
 
   return <section className="section-center">
     {meals.map((singleMeal) => {
-      const { idMeal, strMeal:title, strMealThumb:image } = singleMeal
+      const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
 
       return <article key={idMeal} className="single-meal" >
-        <img src={image} style={{ width: '200px' }} className="img" />
+        <img src={image} className="img" />
         <footer>
           <h5>{title}</h5>
-          <button className='like-btn'>click me</button>
+          <button className='like-btn'><BsHandThumbsUp /></button>
         </footer>
 
       </article>
